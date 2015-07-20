b0d51d52fa593828a86242527306fcfbc6b79387
manuel <mdp.webdeveloper@gmail.com>
2015-07-20 19:20:25 +0200

    configuracion de la base de datos

--- wordpress/wp-config-sample.php
+++ wordpress/wp-config-sample.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'u923399932_rah');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'u923399932_rah');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', 'password');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'mysql.hostinger.es');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
